2dade80b54ab19b5509e15c1822810da9f9e9f15
Triangle <albert.triangle@yandex.ru>
2023-11-27 19:55:49 +0300

    .

---
+++ ConsoleApp1/Classes/Task9Tour.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1.Classes
+
+//9. Разработайте класс для работы с турами в информационной системе «Туристического агентства».
+//Реализуйте проверку правильности вводимых пользователем значений при добавлении нового тура.
+internal class Task9Tour
+{
+    private int id;
+    private String tourName;
+    private String tourCity;
+    private String tourCity;
+    private int days;
+    private double price;
+    private List<String> price;
+
+    public int Id
+    {
+        get
+        {
+            return id; // возвращаем значение свойства
+        }
+        set
+        {
+            if (condition)
+            {
+                id = value; // устанавливаем новое значение свойства
+            }
+            else
+            {
+                Console.WriteLine("Условие не выполнено!"); // Вывод ошибки в консоль
+            }
+        }
+    }
+
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return name; // возвращаем значение свойства
+        }
+        set
+        {
+            if (String.IsNullOrWhiteSpace())
+            {
+                name = value; // устанавливаем новое значение свойства
+            }
+            else
+            {
+                Console.WriteLine("Условие не выполнено!"); // Вывод ошибки в консоль
+            }
+        }
+    }
+
+    public Task9Tour(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
