ac881b7351f7a6cd3d6b3d5aac7752cde7a21784
ION <thegoshagames@gmail.com>
2023-11-27 19:55:19 +0300

    ♦

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,62 +7,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class AppObject
-    {
-        private int id;
-        public int Id
-        {
-            get
-            {
-                return id; // возвращаем значение свойства
-            }
-            set
-            {
-                if (condition)
-                {
-                    id = value; // устанавливаем новое значение свойства
-                }
-                else
-                {
-                    Console.WriteLine("Условие не выполнено!"); // Вывод ошибки в консоль
-                }
-            }
-        }
-
-        private string name;
-        public string Name
-        {
-            get
-            {
-                return name; // возвращаем значение свойства
-            }
-            set
-            {
-                if (String.IsNullOrWhiteSpace())
-                {
-                    name = value; // устанавливаем новое значение свойства
-                }
-                else
-                {
-                    Console.WriteLine("Условие не выполнено!"); // Вывод ошибки в консоль
-                }
-            }
-        }
-
-        public AppObject(int id, string name)
-        {
-            this.id = id;
-            this.name = name;
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            AppObject example = new AppObject(0, "name");
-
-            example.Name = "name";
+            // Задание #10
+            BookObject bookObject = new BookObject(0, "Книга", "Иванов И. И.", 100, 2023);
         }
     }
 }
